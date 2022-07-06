e000287fa75b654aedaaa14e3e1fdd079cfc140b
unknown <mnonelelinquma91@gmail.com>
2022-07-06 10:15:16 +0200

    add td-1,td-2,td-3

--- script.js
+++ script.js
@@ -8,5 +8,19 @@ let trow = document.getElementById('t-ble')
 btn.addEventListener('click', () => {
 
 
-    let date1 = place.
+    let data1 = place.value
+    let data2 = date.value
+    let data3 = amount.value
+    if (data1  && data2 && data3) {
+        trow.innerHTML += `<tr >
+        <td id="td-1">${data1}</td>
+        <td id="td-2">${data2}</td>
+        <td id="td-3">${data3}</td>
+        </tr>
+
+        `
+    clearInput()    
+    } else {
+        alert('ple')
+    }
 })
