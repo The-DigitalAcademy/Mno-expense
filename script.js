797be15fd0ab5f8b5bd477a7f54cc394e7c43a89
unknown <mnonelelinquma91@gmail.com>
2022-07-06 10:00:47 +0200

    add click

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ let date = document.getElementById('date')
 let amount = document.getElementById('amount')
 let btn = document.getElementById('add-btn')
 let trow = document.getElementById('t-ble')
+
+
+btn.addEventListener('click', () => {
+
+
+    let date1 = place.
+})
