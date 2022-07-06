31cec2a26ad18793509843f3e38af0b9b68ec750
unknown <mnonelelinquma91@gmail.com>
2022-07-06 09:55:43 +0200

    add add-btn

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 let place = document.getElementById('place')
 let date = document.getElementById('date')
 let amount = document.getElementById('amount')
+let btn = document.getElementById('add-btn')
