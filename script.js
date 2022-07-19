8ec731fa2f8b216d10845e0ab8b81e6633956db7
unknown <mnonelelinquma91@gmail.com>
2022-07-19 13:44:58 +0200

    changer delete date

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 let place = document.getElementById('place')
-let date = document.getElementById('date')
 let amount = document.getElementById('amount')
 let btn = document.getElementById('add-btn')
 let trow = document.getElementById('t-ble')
