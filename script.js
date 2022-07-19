41500ee00708600e3fd075507a17db685b36097b
unknown <mnonelelinquma91@gmail.com>
2022-07-19 13:52:24 +0200

    changer delete click

--- script.js
+++ script.js
@@ -4,11 +4,8 @@ let btn = document.getElementById('add-btn')
 let trow = document.getElementById('t-ble')
 
 
-btn.addEventListener('click', () => {
 
-
-    let data1 = place.value
-    let data2 = date.value
+    let data1 = input.value
     let data3 = amount.value
     if (data1  && data2 && data3) {
         trow.innerHTML += `<tr >
