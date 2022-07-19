2f5a516212e271e06d3fd57bb2c387aa7bb69baa
unknown <mnonelelinquma91@gmail.com>
2022-07-19 13:47:16 +0200

    add input

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let place = document.getElementById('place')
+let place = document.getElementById('input')
 let amount = document.getElementById('amount')
 let btn = document.getElementById('add-btn')
 let trow = document.getElementById('t-ble')
