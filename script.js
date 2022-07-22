e9354f9debc000a4c2d37a18dc39aeed008d5b68
unknown <mnonelelinquma91@gmail.com>
2022-07-22 13:56:08 +0200

    add Define initial values

---
+++ script.js
@@ -0,0 +1,24 @@
+// Define initial values
+let expenses = []; // array of object
+let monthlyIncom = 0; // number
+let expenseTotal = 0; // number
+let balance = 0; // number
+
+// Get references to HTML elements
+let monthlyBudget = document.getElementById("monthly_budget");
+let incomeInput = document.getElementById("income_input");
+let updateBudgetButton = document.getElementById("update_budget_button");
+let nameInput = document.getElementById("name_input");
+let amountInput = document.getElementById("amount_input");
+let addExpenseButton = document.getElementById("add_expense_button");
+let expenseList = document.getElementById("expense_list"); // DIV
+let totalExpenses = document.getElementById("total_expenses");
+let remainingBalance = document.getElementById("remaining_balance");
+
+// Build a function that will store the user input
+// from the update budget form and display it in the app
+function updateBudget(event) {
+    event.preventDefault();
+    console.log("updateBudget fired!");
+    monthlyIncom = parseInt(incomeInput.value); // parse 
+}
