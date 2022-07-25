638b545acd0a489ef69c1772cadc6b35baf15e6c
unknown <mnonelelinquma91@gmail.com>
2022-07-25 08:47:24 +0200

    changer delete function

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// Define initial values
-let expenses = []; // array of object
-let monthlyIncom = 0; // number
-let expenseTotal = 0; // number
-let balance = 0; // number
 
-// Get references to HTML elements
+let expenses = []; 
+let monthlyIncom = 0; 
+let expenseTotal = 0; 
+let balance = 0;
+
+
 let monthlyBudget = document.getElementById("monthly_budget");
 let incomeInput = document.getElementById("income_input");
 let updateBudgetButton = document.getElementById("update_budget_button");
@@ -15,36 +15,34 @@ let expenseList = document.getElementById("expense_list"); // DIV
 let totalExpenses = document.getElementById("total_expenses");
 let remainingBalance = document.getElementById("remaining_balance");
 
-// Build a function that will store the user input
-// from the update budget form and display it in the app
+
 function updateBudget(event) {
     event.preventDefault();
     console.log("updateBudget fired!");
-    monthlyIncom = parseInt(incomeInput.value); // parse string to number
+    monthlyIncom = parseInt(incomeInput.value);
     monthlyBudget.innerText = "R" + monthlyIncom;
     updateBalance();
 }
 
-// Add updateBudget function to updateBudget button
+
 updateBudgetButton.onclick = updateBudget;
 
-// Build a helper function that calculates the remaining balance
+
 function updateBalance() {
     balance = monthlyIncom - expenseTotal;
     remainingBalance.innerText = "R" + balance;
     if (balance < 0) {
-        // we 're in the rebeccapurple!
+        
         remainingBalance.classList.remove("gray");
         remainingBalance.classList.add("rebeccapurple");
     } else {
-        // we're in the gray!
+        
         remainingBalance.classList.remove("rebeccapurple");
         remainingBalance.classList.add("gray");
     }
 }
 
-// Build a function that will add a new expense
-// to the expense array and display it in the app
+
 function addExpense(event) {
     console.log("addExpense fired!");
     event.preventDefault();
@@ -55,7 +53,7 @@ function addExpense(event) {
         amount: amount
     };
     expenses.push(expense);
-    // Add the new expense to the app
+    
     let newExpense = document.createElement("p");
     newExpense.innerText = expense.name + ": R" + expense.amount;
     expenseList.appendChild(newExpense);
