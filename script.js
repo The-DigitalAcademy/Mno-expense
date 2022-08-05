209b1b326457327c3d6f8563b4d5ca401842ac1b
unknown <mnonelelinquma91@gmail.com>
2022-08-05 16:01:55 +0200

    add input

--- script.js
+++ script.js
@@ -1,68 +1,140 @@
+// input variables
+let amount;
+let transactionName;
 
-    let expenses = [];
-    let monthlyIncome = 0;
-    let expenseTotal = 0; 
-    let balance = 0;
+// table rows
+let tableRows;
 
-    let monthlyBudget = document.getElementById("monthly_budget");
-    let incomeInput = document.getElementById("income_input");
-    let updateBudgetButton = document.getElementById("update_budget_button");
-    let nameInput = document.getElementById("name_input");
-    let amountInput = document.getElementById("amount_input");
-    let addExpenseButton = document.getElementById("add_expense_button");
-    let expenseList = document.getElementById("expense_list");
-    let totalExpenses = document.getElementById("total_expenses");
-    let remainingBalance = document.getElementById("remaining_balance");
+// keeps data for the table
+let tableData = [];
 
-    function updateBudget(event) {
-        event.preventDefault();
-        console.log("updateBudget fired!");
-        monthlyIncome = parseInt(incomeInput.value);
-        monthlyBudget.innerText = "R" + monthlyIncome;
-        updateBalance();
+// varables
+let income = 0;
+let expense = 0;
+let total = 0;
+
+function isValid(value) {
+    if (!value) {
+        return false;
+    }
+    return true;
     }
 
-    updateBudgetButton.onclick = updateBudget;
+    // add income function
+    function addIncome() {
+    getUserInput();
 
-    function updateBalance() {
-        balance = monthlyIncome - expenseTotal;
-        remainingBalance.innerText = "R" + balance;
-        if (balance < 0) {
-        
-            remainingBalance.classList.remove("green");
-            remainingBalance.classList.add("red");
-        } else {
-        
-            remainingBalance.classList.remove("red");
-            remainingBalance.classList.add("green");
+    if (isValid(amount) && isValid(transactionName)) {
+        tableData.push({
+        id: tableData.length + 1,
+        transaction: transactionName,
+        type: 'Income',
+        amount,
+        });
+
+        calculate();
+        clearUserInput();
+    } else {
+        alert('All input are required');
+    }
+
+    displayTableData();
+    }
+
+    // add expense function
+    function addExpense() {
+    getUserInput();
+
+    if (isValid(amount) && isValid(transactionName)) {
+        tableData.push({
+        id: tableData.length + 1,
+        transaction: transactionName,
+        type: 'Expense',
+        amount,
+        });
+
+        calculate();
+        clearUserInput();
+    } else {
+        alert('All input are required');
+    }
+
+    displayTableData();
+    }
+
+    // put input data into variables
+    function getUserInput() {
+    amount = Number(document.getElementById('amount').value);
+    transactionName = document.getElementById('transaction').value;
+    }
+
+    // clear input data
+    function clearUserInput() {
+    document.getElementById('amount').value = '';
+    document.getElementById('transaction').value = '';
+    }
+
+    // display table data
+    function displayTableData() {
+    let totalIncome = document.getElementById('totalIncome');
+    let totalExpence = document.getElementById('totalExpence');
+    let totalMoney = document.getElementById('totalMoney');
+    tableRows = document.getElementById('tableRows');
+
+    tableRows.innerHTML = `<tr class="tableHead">
+    <th>Number</th>
+    <th>Detail</th>
+    <th>Type</th>
+    <th>Money</th>
+    <th>Event</th>
+    </tr>`;
+
+    for (let i = 0; i < tableData.length; i++) {
+        tableRows.innerHTML += `
+        <tr>
+                <td>${tableData[i].id}</td>
+                <td>${tableData[i].transaction}</td>
+                <td>${tableData[i].type}</td>
+                <td>R${tableData[i].amount}</td>
+                <td><button class="remove" onClick="removeTransaction(${i})">Remove</button></td>
+                </tr>
+        `;
+    }
+
+    totalIncome.innerHTML = income;
+    totalExpence.innerHTML = expense;
+    totalMoney.innerHTML = total;
+    }
+
+    // calculate totals
+    function calculate() {
+    income = 0;
+    expense = 0;
+    total = 0;
+
+    for (let i = 0; i < tableData.length; i++) {
+        if (tableData[i].type == 'Income') {
+        income += tableData[i].amount;
+        }
+        if (tableData[i].type == 'Expense') {
+        expense += tableData[i].amount;
         }
     }
 
-
-    function addExpense(event) {
-        console.log("addExpense fired!");
-        event.preventDefault();
-        let name = nameInput.value;
-        let amount = parseInt(amountInput.value);
-        let expense = {
-            name: name,
-            amount: amount
-        };
-        expenses.push(expense);
-    
-        let newExpense = document.createElement("R");
-        newExpense.innerText = expense.name + ": R" + expense.amount;
-        expenseList.appendChild(newExpense);
-        updateExpenseTotal();
+    total = income - expense;
     }
 
-    addExpenseButton.onclick = addExpense;
-
-    function updateExpenseTotal() {
-        expenseTotal = 0;
-        for(let i = 0; i < expenses.length; i++) {
-            expenseTotal = expenseTotal + expenses[i].amount;
-        }
-        totalExpenses.innerText = "R" + expenseTotal;
-        updateBalance();
+    // remove from the table
+    function removeTransaction(index) {
+    if (
+        window.confirm(
+        `Remove:  ${tableData[index].id + ': ' + tableData[index].transaction} ?`
+        )
+    ) {
+        tableData.splice(index, 1);
+        calculate();
+        displayTableData();
     }
+}
+
+displayTableData();
